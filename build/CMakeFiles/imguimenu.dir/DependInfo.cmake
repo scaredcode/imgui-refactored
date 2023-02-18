00196510b85bb8b9dac7af91895166b33077175e
scaredcode <r3s1zetv@gmail.com>
2023-02-18 23:16:08 +0300

    Unix Makefiles add lol

---
+++ build/CMakeFiles/imguimenu.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/backends/imgui_impl_opengl3.cpp" "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/backends/imgui_impl_sdl2.cpp" "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_sdl2.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/imgui.cpp" "CMakeFiles/imguimenu.dir/imgui-special/imgui.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/imgui.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/imgui_demo.cpp" "CMakeFiles/imguimenu.dir/imgui-special/imgui_demo.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/imgui_demo.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/imgui_draw.cpp" "CMakeFiles/imguimenu.dir/imgui-special/imgui_draw.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/imgui_draw.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/imgui_tables.cpp" "CMakeFiles/imguimenu.dir/imgui-special/imgui_tables.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/imgui_tables.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/imgui-special/imgui_widgets.cpp" "CMakeFiles/imguimenu.dir/imgui-special/imgui_widgets.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/imgui-special/imgui_widgets.cpp.o.d"
+  "/home/q/dev/imgui-refactored/sdl-opengl/main.cpp" "CMakeFiles/imguimenu.dir/main.cpp.o" "gcc" "CMakeFiles/imguimenu.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/imguimenu.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_sdl2.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/backends/imgui_impl_sdl2.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_demo.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_demo.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_draw.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_draw.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_tables.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_tables.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_widgets.cpp.o"
+  "CMakeFiles/imguimenu.dir/imgui-special/imgui_widgets.cpp.o.d"
+  "CMakeFiles/imguimenu.dir/main.cpp.o"
+  "CMakeFiles/imguimenu.dir/main.cpp.o.d"
+  "imguimenu"
+  "imguimenu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imguimenu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
